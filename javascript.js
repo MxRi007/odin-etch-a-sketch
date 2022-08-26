3eab2379ec71212836326e17d1d9d7c8dd3b5ad5
Richard E. Iniengo <iniengori@gmail.com>
2022-08-26 18:19:42 +0200

    Add a button that enables gradual darkening

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
 const editor = document.querySelector('#editor');
 const btnSize = document.querySelector('#gridSize');
 const btnColor = document.querySelector('#color');
+const btnRandomColors = document.querySelector('#randomColors');
+const btnGradualColors = document.querySelector('#gradualColors');
+
+
+let randomColors = false;
+let gradualColors = false;
+
+
+btnRandomColors.addEventListener('click', () => {
+    randomColors = !randomColors;
+    randomColors ? 
+    btnRandomColors.textContent = 'random colors: on' :
+    btnRandomColors.textContent = 'random colors: off';
+
+});
+
+btnGradualColors.addEventListener('click', () => {
+    gradualColors = !gradualColors;
+    gradualColors ? 
+    btnGradualColors.textContent = 'gradual colors: on' :
+    btnGradualColors.textContent = 'gradual colors: off';
+
+});
+
 
 btnSize.addEventListener('click', promptGrid);
 
@@ -44,11 +68,25 @@ function buildGrid(length){
             if(mousedown){
                 setColor(square);
             }
+
+            if(gradualColors){
+                let rgb = square.style.backgroundColor.slice(4,-1).split(',');
+                let r = rgb[0];
+                let g = rgb[1];
+                let b = rgb[0];
+
+                square.style.backgroundColor = `rgb(${r-25},${g-25}, ${b-25} )`;
+            }
         });
 
         square.addEventListener('mousedown', () => {
-            setColor(square);
-            mousedown = true;
+            if(!gradualColors){
+                setColor(square);
+                mousedown = true;
+                
+            }
+
+
         });
 
         square.addEventListener('mouseup', () => {
@@ -59,13 +97,25 @@ function buildGrid(length){
 
         }
     }
-    console.log(coordinates);
 }
 
 function setColor(element){
-    element.style.backgroundColor = btnColor.value;
+    if(randomColors){
+        element.style.backgroundColor = getRandomColor();
+    }  else {
+        element.style.backgroundColor = btnColor.value;
+    }
+
 }
 
+function getRandomColor(){
+    let r = Math.round((Math.random() *255) + 1);
+    let g = Math.round((Math.random() *255) + 1);
+    let b = Math.round((Math.random() *255) + 1);
+
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 buildGrid(40);
 
