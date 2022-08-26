6a2fb50899da334300a162313db9d5c67947eb06
Richard E. Iniengo <iniengori@gmail.com>
2022-08-26 11:12:11 +0200

    Add a png frame to the editor display

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ btnSize.addEventListener('click', promptGrid);
 function promptGrid(){
     let size = +prompt('Please enter a grid size between 10 - 100 pixels.');
     while(true){
-        if(!size){
+        if(size === null || size === ''){
             break;
         } else if(isNaN(size)){
             size = +prompt('That\'s not a number. Please enter a number between 10 - 100 pixels.');
@@ -25,10 +25,17 @@ function promptGrid(){
 }
 
 let mousedown = false;
+let coordinates = [];
 
 function buildGrid(length){
-    for(let i = 0; i < length*length; i++){
+    for(let i = 1; i <= length; i++){
+        for(let j = 1; j <= length; j++){
+            
+       
         const square = document.createElement('div');
+        
+        coordinates.push([i, j, square]);
+        
         square.classList.add('square');
         square.style.width = `${960/length}px`;
         square.style.height = `${960/length}px`;
@@ -49,7 +56,10 @@ function buildGrid(length){
         });
 
         editor.appendChild(square);
+
         }
+    }
+    console.log(coordinates);
 }
 
 function setColor(element){
